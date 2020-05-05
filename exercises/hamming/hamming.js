831a35b602324198127f945cbad927e5d51aece6
Noa Karni <shirakarni32@gmail.com>
2020-05-05 16:18:03 +0300

    Added tests for Hamming exercise

--- exercises/hamming/hamming.js
+++ exercises/hamming/hamming.js
@@ -1,8 +1,18 @@
-//
-// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const compute = (strands) => {
+  const len1 = strands[0].length;
+  const len2 = strands[1].length;
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+  if (!len1 && len2) {
+    throw new Error('left strand must not be empty');
+  }
+
+  if (len1 && !len2) {
+    throw new Error('right strand must not be empty');
+  }
+
+  if (len1 !== len2) {
+    throw new Error('left and right strands must be of equal length');
+  }
+
+  return [...strands[0]].filter((element, index) => element !== strands[1][index]).length;
 };
